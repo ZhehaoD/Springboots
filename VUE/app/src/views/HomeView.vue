269c29e2d360ea3709291cbd6193d57fffecb7ec
ZhehaoD <zhehaodn@163.com>
2025-09-28 20:58:06 +0800

    font-end

--- VUE/app/src/views/HomeView.vue
+++ VUE/app/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div style="width: 100%">
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside style="width: 200px; min-height: 100vh; background-color: #001529">
+      <el-aside :width="asideWidth" style="min-height: 100vh; background-color: #001529">
         <div
             style="
             height: 60px;
@@ -12,26 +12,21 @@
             justify-content: center;
           "
         >
-          logo
+          <img src="@/assets/logo.webp" style="width: 40px; height: 40px">
+          <span class="logo" v-show="!isCollapse">honey2025</span>
         </div>
-        <el-menu router style="border: none" :default-active="$route.path" background-color="#001529" text-color="rgba(255,255,255,0.65)" active-text-color="#fff" >
+        <el-menu  :collapse="isCollapse" :collapse-transition="false" router style="border: none" :default-active="$route.path" background-color="#001529" text-color="rgba(255,255,255,0.65)" active-text-color="#fff" >
           <el-menu-item index="/">
-            <template v-slot:title>
-              <el-icon><House /></el-icon>
-              <span>系统首页</span>
-            </template>
+            <el-icon><House /></el-icon>
+            <span>系统首页</span>
           </el-menu-item>
           <el-menu-item index="/1">
-            <template v-slot:title>
-              <el-icon><House /></el-icon>
-              <span>系统首页</span>
-            </template>
+            <el-icon><House /></el-icon>
+            <span>系统首页</span>
           </el-menu-item>
           <el-menu-item index="/2">
-            <template v-slot:title>
-              <el-icon><House /></el-icon>
-              <span>系统首页</span>
-            </template>
+            <el-icon><House /></el-icon>
+            <span>系统首页</span>
           </el-menu-item>
           <el-sub-menu>
             <!-- 正确的插槽使用方式，v-slot:title 表示使用名为 title 的插槽 -->
@@ -39,32 +34,108 @@
               <el-icon><Menu /></el-icon>
               <span>信息管理</span>
             </template>
-            <el-menu-item>用户信息</el-menu-item>
+            <el-menu-item  index="/">用户信息</el-menu-item>
             <el-menu-item>管理员信息</el-menu-item>
           </el-sub-menu>
         </el-menu>
       </el-aside>
       <el-container>
         <!-- 头部区域 -->
-        <el-header>头部区域</el-header>
+        <el-header>
+
+          <el-icon style="font-size: 35px; cursor: pointer;" @click="handleCollapse">
+            <!-- 根据折叠状态动态显示不同图标 -->
+            <template v-if="isCollapse">
+              <Expand /> <!-- 折叠状态时显示"展开"图标 -->
+            </template>
+            <template v-else>
+              <Fold /> <!-- 展开状态时显示"折叠"图标 -->
+            </template>
+          </el-icon>
+
+          <el-breadcrumb style="margin-left: 20px">
+            <el-breadcrumb-item :to="{path:'/'}">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{path:'/user'}">用户管理</el-breadcrumb-item>
+          </el-breadcrumb>
+
+          <div style="flex: 1; width: 0; display: flex; align-items: center; justify-content: end;">
+            <el-icon style="font-size: 20px; margin: 5px" @click="handleFull"><FullScreen /></el-icon>
+            <el-dropdown>
+              <div style="display: flex; align-items: center; justify-content: end;">
+                <img
+                    src="@/assets/logo.webp"
+                    style="width: 40px; height: 40px; cursor: default"
+                alt="用户头像"
+                >
+                <span>管理员</span>
+              </div>
+              <template v-slot:dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>个人信息</el-dropdown-item>
+                  <el-dropdown-item>修改密码</el-dropdown-item>
+                  <el-dropdown-item>退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
+        </el-header>
+
         <!-- 主体区域 -->
-        <el-main>主体区域</el-main>
+        <el-main>
+          <div style="box-shadow: 0 0 10px rgba(0,0,0,.1); padding: 10px 20px; border-radius: 5px; margin-bottom: 10px">
+            早安，骚年，祝你开心每一天！
+          </div>
+          <el-card style="width: 500px">
+            <div class="clearfix">
+              <span>毕设2025</span>
+            </div>
+            <div>
+              2025毕设正式开始了！
+              <div style="margin-top: 20px">
+                <div style="margin: 10px 0"><strong>主题色</strong></div>
+                <el-button type="primary">按钮</el-button>
+                <el-button type="success">按钮</el-button>
+                <el-button type="warning">按钮</el-button>
+                <el-button type="danger">按钮</el-button>
+                <el-button type="info">按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
-import {House, Menu} from "@element-plus/icons-vue";
-import {defineComponent} from "vue";
+import {House, Menu,Expand,Fold,FullScreen} from "@element-plus/icons-vue";
 
-export default defineComponent({
-  components: {Menu, House}
+export default ({
+  name:"HomeView",
+  data(){
+    return {
+      isCollapse:false,
+      asideWidth:'200px',
+    }
+  },
+  methods:{
+    handleFull(){
+      document.documentElement.requestFullscreen()
+    },
+    handleCollapse(){
+      this.isCollapse =! this.isCollapse
+      this.asideWidth = this.isCollapse ? '64px' : '200px'
+    }
+  },
+  components: {Menu, House,Expand,Fold,FullScreen}
 })
 
 </script>
 
 <style>
+  body{
+    margin: 0;
+  }
   .el-menu--inline .el-menu-item{
     background-color: #000c17 !important;
     padding-left: 50px !important;
@@ -75,15 +146,41 @@ export default defineComponent({
   }
 
   .el-menu-item.is-active{
-    background-color: #40a9ff;
+    padding: 20px 20px 20px 16px!important;
+    background-color: #40a9ff !important;
     border-radius: 5px !important;
-    margin: 4px;
+    width:calc(100% - 8px);
+    margin-left: 4px;
+  }
+  .el-menu.el-menu--inline .el-menu-item.is-active{
+    padding: 20px 20px 20px 46px!important;
+
   }
 
   .el-menu-item, .el-sub-menu__title{
     height: 40px !important;
     line-height: 40px !important;
-    margin: 4px;
   }
-  
+
+  .el-sub-menu .el-menu-item{
+    min-width: 0 !important;
+  }
+
+  .el-aside{
+    transition: width .3s;
+    box-shadow: 2px 0 6px rgba(0,21,41,.35);
+  }
+
+  .logo{
+    margin-left: 5px;
+    font-size: 20px;
+    transition: all .3s;
+  }
+
+  .el-header{
+    box-shadow: 2px 0 6px rgba(0,21,41,.35);
+    display: flex;
+    align-items: center;
+  }
+
 </style>
